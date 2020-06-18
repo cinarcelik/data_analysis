39e49318953f6eee93b74246a1e3795bfa227b57
Çınar Çelik <59706127+cinarcelik@users.noreply.github.com>
2020-06-18 14:56:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello World")
+"""
+Data Analysis Project's main executer file.
+Written by Çınar Çelik.
+Turkey - 2020
+"""
+
+import sqlite3
+import ssl
+import urllib.error
+import urllib.parse
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+
+url = "https://competition.adesignaward.com/winners.php"
+
+with urllib.request.urlopen(url, context=ctx) as response:
+    html = response.read()
+
+    # if html.getcode() != 200:
+    #     print("Error code=", html.getcode(), url)
+    #     quit()
+
+    print(html)
